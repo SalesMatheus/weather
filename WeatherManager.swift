665f820c453ac867540fbdb9ffc3f61b0e08f0ec
Matheus Sales <matheus.carvalho.sales@gmail.com>
2022-09-28 09:54:15 -0300

    feat: get location data

--- WeatherManager.swift
+++ WeatherManager.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CoreLocation
 
 protocol WeatherManagerDelegate {
     func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel)
@@ -21,6 +22,11 @@ struct WeatherManager {
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(with: urlString)
+    }
+    
+    func fetchWeather(latitute: CLLocationDegrees, longitude: CLLocationDegrees) {
+        let urlString = "\(weatherURL)&lat=\(latitute)&lon=\(longitude)"
         performRequest(with: urlString)
     }
     
